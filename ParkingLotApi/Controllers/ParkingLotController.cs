343933fcc1d74c6d8489debbdce3e1feb6fee802
Zejie Yao <ZYao3@slb.com>
2022-11-05 13:58:56 +0800

    feat: add ParkingLotService and ParkingLotController

---
+++ ParkingLotApi/Controllers/ParkingLotController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingLotApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParkingLotController : ControllerBase
+    {
+        [HttpGet]
+        public string GetParkingLots()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ ParkingLotApi/Services/ParkingLotService.cs
@@ -0,0 +1,14 @@
+﻿using ParkingLotApi.Repository;
+
+namespace ParkingLotApi.Services
+{
+    public class ParkingLotService
+    {
+        private readonly ParkingLotContext parkingLotContext;
+
+        public ParkingLotService(ParkingLotContext parkingLotContext)
+        {
+            this.parkingLotContext = parkingLotContext;
+        }
+    }
+}
