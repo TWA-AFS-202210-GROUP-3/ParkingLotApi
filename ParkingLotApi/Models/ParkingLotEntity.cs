f37e9c7fd59a191c6a3364f7b7005a1589a4d111
Zejie Yao <ZYao3@slb.com>
2022-11-05 13:45:55 +0800

    feat: build ParkingLotEntity and ParkingLotDto

---
+++ ParkingLotApi/Models/ParkingLotEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingLotApi.Models
+{
+    public class ParkingLotEntity
+    {
+        public ParkingLotEntity() {}
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string Location { get; set; }
+    }
+}
