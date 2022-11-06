7e91f621c1d56e3ad5ce6f7acbe73e4cd87914e2
Fengjun Ma <FMa6@slb.com>
2022-11-06 19:41:50 +0800

    test: add unit test of createOneOrder

---
+++ ParkingLotApiTest/ServiceTest/OrderServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ParkingLotApi.Dto;
+using ParkingLotApi.Repository;
+using ParkingLotApi.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotApiTest.ServiceTest
+{
+    [Collection("test")]
+    public class OrderServiceTest : TestBase
+    {
+        public OrderServiceTest(CustomWebApplicationFactory<Program> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_Create_order_successfully()
+        {
+            //given
+            var context = GetDbContext();
+
+            ParkingLotDto parkingLotDto = new ParkingLotDto()
+            {
+                Name = "ParkingLot1",
+                Capacity = 100,
+                Location = "North street No 1",
+            };
+            var parkingLotService = new ParkingLotService(context);
+            await parkingLotService.AddOneParkingLot(parkingLotDto);
+
+            OrderDto orderDto = new OrderDto()
+            {
+                OrderNumber = "Order1",
+                ParkingLotName = "ParkingLot1",
+                PlateNumber = "A1B2C3",
+                Status = true,
+            };
+            var orderService = new OrderService(context);
+
+            //when
+            await orderService.AddOneOrder(orderDto);
+
+            //then
+            Assert.Equal(1, context.orders.Count());
+        }
+
+        public ParkingLotContext GetDbContext()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedService = scope.ServiceProvider;
+            ParkingLotContext context = scopedService.GetService<ParkingLotContext>();
+            return context;
+        }
+    }
+}
