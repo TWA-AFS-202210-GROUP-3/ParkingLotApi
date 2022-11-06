a6d66609c9690e13cb507c33972b54dee3825daa
Fengjun Ma <FMa6@slb.com>
2022-11-06 19:49:39 +0800

    test: add unit test of delete all

--- ParkingLotApiTest/ServiceTest/OrderServiceTest.cs
+++ ParkingLotApiTest/ServiceTest/OrderServiceTest.cs
@@ -78,12 +78,66 @@ namespace ParkingLotApiTest.ServiceTest
             Assert.Equal(orderDto.OrderNumber, orderDtoGet.OrderNumber);
         }
 
+        [Fact]
+        public async Task Should_delete_all_order_successfully()
+        {
+            //given
+            var context = GetDbContext();
+            AddMultiOrder(context);
+            var orderService = new OrderService(context);
+            //when
+            orderService.DeleteAllOrder();
+            //then
+            Assert.Equal(0, context.orders.Count());
+        }
+
         public ParkingLotContext GetDbContext()
         {
             var scope = Factory.Services.CreateScope();
             var scopedService = scope.ServiceProvider;
             ParkingLotContext context = scopedService.GetService<ParkingLotContext>();
             return context;
+        }
+
+        public void AddMultiOrder(ParkingLotContext context)
+        {
+            ParkingLotDto parkingLotDto = new ParkingLotDto()
+            {
+                Name = "ParkingLot1",
+                Capacity = 100,
+                Location = "North street No 1",
+            };
+            var parkingLotService = new ParkingLotService(context);
+            parkingLotService.AddOneParkingLot(parkingLotDto);
+
+            OrderDto orderDto1 = new OrderDto()
+            {
+                OrderNumber = "Order1",
+                ParkingLotName = "ParkingLot1",
+                PlateNumber = "9999999",
+                Status = true,
+            };
+            OrderDto orderDto2 = new OrderDto()
+            {
+                OrderNumber = "Order2",
+                ParkingLotName = "ParkingLot1",
+                PlateNumber = "8888888",
+                Status = true,
+            };
+            OrderDto orderDto3 = new OrderDto()
+            {
+                OrderNumber = "Order3",
+                ParkingLotName = "ParkingLot1",
+                PlateNumber = "7777777",
+                Status = true,
+            };
+
+            var orderService = new OrderService(context);
+            List<OrderDto> orderDtos = new List<OrderDto>() { orderDto1, orderDto2, orderDto3 };
+            foreach (OrderDto orderDto in orderDtos)
+            {
+                orderService.AddOneOrder(orderDto);
+            }
         }
     }
 }
