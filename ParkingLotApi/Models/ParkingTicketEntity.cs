ac682c8c6189c280a965f9005b266ac705493c09
Zejie Yao <ZYao3@slb.com>
2022-11-06 19:21:55 +0800

    feat: add ParkingTicket dto and entity

---
+++ ParkingLotApi/Models/ParkingTicketEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace ParkingLotApi.Models
+{
+    public class ParkingTicketEntity
+    {
+        public ParkingTicketEntity() {}
+
+        public int ID { get; set; }
+
+        public string ParkingLotName { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public string CreateTime { get; set; }
+
+        public string CloseTime { get; set; }
+
+        public bool OrderStatus { get; set; }
+    }
+}
