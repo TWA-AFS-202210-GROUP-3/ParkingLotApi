9fb6d81c67591f5bc1e961f79de0ebfe59b42567
hdeng <hdeng10@slb.com>
2022-11-07 19:22:14 +0800

    feat: can_update_parking_order_infor_sucessfully

--- ParkingLotApiTest/ControllerTest/OrderTest.cs
+++ ParkingLotApiTest/ControllerTest/OrderTest.cs
@@ -54,5 +54,45 @@ namespace ParkingLotApiTest.ControllerTest
 
             Assert.Equal("BJ-897276",NewParkingOrder.PlateNumber);
         }
+
+        [Fact]
+        public async Task Should_update_parking_order_infor_sucessfully()
+        {
+            //given
+            var client = GetClient();
+            ParkingLotDto parkingLotDto = new ParkingLotDto()
+            {
+                Name = "Lot_2",
+                Capacity = 60,
+                Location = "Strict No.1 ",
+            };
+            var httpContent = JsonConvert.SerializeObject(parkingLotDto);
+            StringContent content = new StringContent(httpContent, Encoding.UTF8, MediaTypeNames.Application.Json);
+            await client.PostAsync("/parkinglots", content);
+
+            OrderDto orderDto = new OrderDto()
+            {
+                ParkingLotName = parkingLotDto.Name,
+                PlateNumber = "BJ-897276",
+                CreationTime = DateTime.Now,
+                Status = true,
+            };
+            var httpContent2 = JsonConvert.SerializeObject(orderDto);
+            StringContent content2 = new StringContent(httpContent2, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var postOrderResponse =await client.PostAsync("/parkingorders", content2);
+
+            //when
+            orderDto.CloseTime =DateTime.Now;
+            orderDto.Status = false;
+            var httpContent3 = JsonConvert.SerializeObject(orderDto);
+            StringContent content3 = new StringContent(httpContent3, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var putOrderResponse = await client.PutAsync(postOrderResponse.Headers.Location, content3);
+
+            //then
+            var body = await putOrderResponse.Content.ReadAsStringAsync();
+            var UpdatedOrder = JsonConvert.DeserializeObject<OrderDto>(body);
+
+            Assert.False(UpdatedOrder.Status);
+        }
     }
 }
